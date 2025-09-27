232d1117196adc522de2ceea0fec3ea37413da5f
baueranna <anna.bauer511@gmail.com>
2025-09-27 22:10:42 +0200

    datatables changes

--- js/personen_index.js
+++ js/personen_index.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $('#correspondenceTable').DataTable({
-        ajax: '../data/json/person_index.jsdata.json', // URL to your JSON file or API endpoint
+        ajax: '../data/json/person_index.json', // URL to your JSON file or API endpoint
         columns: [
             { data: 'forename' },
             { data: 'surname' },
-            { data: 'affiliation' }
+            { data: 'occupation' }
         ]
     });
 });
